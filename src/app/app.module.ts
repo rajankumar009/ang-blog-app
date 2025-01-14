ba41d4ecacd6fd3f0685e0043e068041b21d3894
rajankumar009 <rk1620703@gmail.com>
2025-01-14 20:49:45 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentFormComponent } from './comment/comment-form/comment-form.component';
+import { CommentListComponent } from './comment/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentFormComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comment/comment-form/comment-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-form',
+  standalone: false,
+  
+  templateUrl: './comment-form.component.html',
+  styleUrl: './comment-form.component.css'
+})
+export class CommentFormComponent {
+
+}
